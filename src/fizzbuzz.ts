8134b79df5d91d4d218a4972d5c8b83b495b0174
Peter Farrar <seven@sevenhuman.net>
2021-08-17 16:56:28 -0400

    Removed commented code

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,7 +1,3 @@
-// export default function fizzBuzz(value: number): string {
-//   const result: string = (value%3 ? '' : 'Fizz') + (value%5 ? '' : 'Buzz')
-//   return (result ? result : null) ?? value.toString()
-// }
 export default function fizzBuzz(value: number): string {
   const result: string = (value%3 ? '' : 'Fizz') + (value%5 ? '' : 'Buzz')
   return result || value.toString()
